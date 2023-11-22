ed6fe69f9efa229747ba4676b4d17f63293e2753
Sam Hamamji <samhamamji0@gmail.com>
2023-11-22 19:18:43 +0200

    Fix empty interval formatiing bug

--- src/interval/interval_formatting.c
+++ src/interval/interval_formatting.c
@@ -9,6 +9,12 @@ static char point_to_char(const struct Point point) {
 }
 
 static char *interval_to_string(const struct Interval *interval) {
+  if (interval->size == 0) {
+    char *string = malloc(sizeof(char) * 15);
+    sprintf(string, "Empty interval");
+    return string;
+  }
+
   char *string = (char *)malloc(2 * interval->size * sizeof(char));
   for (int i = 0; i < interval->size; i++) {
     string[2 * i] = point_to_char(interval->array[i]);
