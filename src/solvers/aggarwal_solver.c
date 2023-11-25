188bfcb77d96b7e76b7e586f4d01d2fb2d481c9a
Sam Hamamji <samhamamji0@gmail.com>
2023-11-25 16:34:11 +0200

    Use sorted exclusion trick in `aggarwal_solver`

--- src/solvers/aggarwal_solver.c
+++ src/solvers/aggarwal_solver.c
@@ -6,22 +6,8 @@
 #include "common/stack.h"
 #include "solver.h"
 
-static int get_initial_exclusion_cost(const struct AlternatingChains *chains,
-                                      int chain_start_index) {
-  int initial_exclusion_cost = -chain_start_index;
-  for (int i = chain_start_index;;
-       i = chains->target_right_partners[chains->source_right_partners[i]]) {
-    if (chains->source_right_partners[i] == NO_RIGHT_PARTNER) {
-      initial_exclusion_cost += i;
-      break;
-    }
-    initial_exclusion_cost += i - chains->source_right_partners[i];
-  }
-  return initial_exclusion_cost;
-}
-
 static int get_exclusion_from_chain(const struct AlternatingChains *chains,
-                                    int chain_start_index) {
+                                    int chain_start_index, int max_index) {
   int best_exclusion_index = chain_start_index;
   int best_exclusion_cost = 0;
 
@@ -29,7 +15,8 @@ static int get_exclusion_from_chain(const struct AlternatingChains *chains,
   int current_exclusion_index = chain_start_index;
 
   while (chains->source_right_partners[current_exclusion_index] !=
-         NO_RIGHT_PARTNER) {
+             NO_RIGHT_PARTNER &&
+         current_exclusion_index < max_index) {
     current_exclusion_cost +=
         chains->source_right_partners[current_exclusion_index] * 2 -
         current_exclusion_index -
@@ -53,10 +40,12 @@ static bool *get_exclusion_from_chains(const struct Interval *const interval,
                                        const struct AlternatingChains *chains,
                                        int imbalance) {
   int *excluded_indexes = malloc(sizeof(int) * imbalance);
+  int max_exclusion_index = interval->size;
 
-  for (int chain_index = 0; chain_index < imbalance; chain_index++) {
+  for (int chain_index = imbalance - 1; chain_index >= 0; chain_index--) {
     excluded_indexes[chain_index] = get_exclusion_from_chain(
-        chains, chains->chain_start_indexes[chain_index]);
+        chains, chains->chain_start_indexes[chain_index], max_exclusion_index);
+    max_exclusion_index = excluded_indexes[chain_index];
   }
 
   bool *exclusion_array = calloc(sizeof(bool), interval->size);
