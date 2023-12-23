8a964655228bc3857954b1e7e28b7f5f5d11389c
Sam Hamamji <samhamamji0@gmail.com>
2023-12-23 14:54:49 +0200

    Setup single performance test in main file

--- src/main.c
+++ src/main.c
@@ -6,19 +6,40 @@
 #include "./lib/mapping/mapping.h"
 #include "./lib/solver/solver.h"
 
+struct Config {
+  int size;
+  int imbalance_percentage;
+  int solver_num;
+  const struct Solver *solvers[];
+};
+
+static const struct Config config = {
+    .size = 10000000,
+    .imbalance_percentage = 15,
+    .solvers = {&aggarwal_solver, &aggarwal_parallel_solver,
+                &aggarwal_parallel_solver_on_chains},
+    .solver_num = 3,
+};
+
 int main() {
   unsigned int seed = (unsigned int)time(NULL);
   srand(seed);
   printf("Seed set to %u\n", seed);
 
-  struct Interval *interval =
-      interval_factory.generate_interval_by_imbalance(20, 16);
-  interval_print(interval);
+  struct timespec start, finish;
+  struct Interval *interval = interval_factory.generate_interval_by_imbalance(
+      config.size, config.size * config.imbalance_percentage / 100);
 
-  struct Mapping *mapping = karp_li_solver.solve(interval);
-  mapping_print(mapping);
+  for (int solver_index = 0; solver_index < config.solver_num; solver_index++) {
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    struct Mapping *mapping = config.solvers[solver_index]->solve(interval);
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    mapping_free(mapping);
+    printf("%s: %lf sec\n", config.solvers[solver_index]->name,
+           (finish.tv_sec - start.tv_sec) +
+               (finish.tv_nsec - start.tv_nsec) / 1000000000.0);
+  }
 
   interval_free(interval);
-  mapping_free(mapping);
   return 0;
 }
